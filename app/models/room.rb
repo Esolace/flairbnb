a336a012d373601278e247d3d092c10bcc498b0e
Solace <esolace@icloud.com>
2018-06-16 17:45:01 +0800

    updated room model

--- app/models/room.rb
+++ app/models/room.rb
@@ -7,5 +7,7 @@ class Room < ApplicationRecord
   validates :bed_room, presence: true
   validates :listing_name, presence: true, length:{maximum: 75}
   validates :summary, presence: true, length:{maximum: 600}
+  validates :address, presence: true
+  validates :price, numericality: {only_integer: true, greater_than: 5}
 
 end
