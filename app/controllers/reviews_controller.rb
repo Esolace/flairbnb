c5fc98e92789b4ecf2337633636232765328720b
Solace <esolace@icloud.com>
2018-06-21 19:50:21 +0800

    reviews improved and showed on user profile page

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,7 +1,7 @@
 class ReviewsController < ApplicationController
 
     def create
-        @review = current_user.reviews.create(reviews_params)
+        @review = current_user.reviews.create(review_params)
         redirect_to @review.room
     end
 
