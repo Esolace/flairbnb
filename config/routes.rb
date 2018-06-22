6e79569de12142f865ebc8371118f9f15bd4fb51
Solace <esolace@icloud.com>
2018-06-22 13:46:49 +0800

    homepage made

--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,7 @@ Rails.application.routes.draw do
   get '/preview' => 'reservations#preview'
   get 'my_trips' => 'reservations#your_trips', as: :your_trips
   get 'my_reservations' => 'reservations#your_reservations', as: :your_reservations
-  get '/search' => 'pages/search'
+  get '/search' => 'pages_controller#search'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
